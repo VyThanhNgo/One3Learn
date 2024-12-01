defc3d49a9c972a9fa216ad7f53211c5699fd5ed
VyThanhNgo <23130393@st.hcmuaf.edu.vn>
2024-12-01 23:05:06 +0700

    Update design.js

--- design.js
+++ design.js
@@ -1,5 +1,5 @@
 function showImage(functionName) {
-    const imageMap = {
+  const imageMap = {
       login: "images/Login.jpg",
       register: "images/Login.jpg",
       forgot_pass: "images/PaswordRecovery.jpg",
@@ -8,9 +8,14 @@ function showImage(functionName) {
       track_progress: "images/Progess.jpg",
       tests: "images/TakeTest.jpg",
       community: "images/CommunityDiscusson.jpg",
-    };
+  };
 
-    const imgElement = document.getElementById('designImage');
-    imgElement.src = imageMap[functionName] || "image/placeholder.png";
-    imgElement.alt = `${functionName.replace('_', ' ')} Design`;
-  }
+  const imgElement = document.getElementById('designImage');
+  
+  // Cập nhật src và alt
+  imgElement.src = imageMap[functionName] || "images/placeholder.png";
+  imgElement.alt = `${functionName.replace('_', ' ')} Design`;
+
+  // Hiển thị hình ảnh khi có hình được chọn
+  imgElement.style.display = "block";
+}
